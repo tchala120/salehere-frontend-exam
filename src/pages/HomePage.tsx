e1b65794624a3b7a140a60d39087f746d6349cc0
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-01-26 17:25:20 +0700

    feat: Added home page to create username

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,48 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import FadeUp from 'components/FadeUp'
+import UserCreationContainer from 'components/UserCreationContainer'
+
+import { routeTo } from 'helpers/utils'
+
+import { paths } from 'setup/PageRouter'
+
 const HomePage = () => {
-  return <>This is home page for create username</>
+  const [username, setUsername] = useState<string>()
+
+  const navigate = useNavigate()
+
+  const startAnimate = username != null && username.length > 0
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault()
+
+        navigate(
+          routeTo(paths.createRoom, {
+            params: {
+              username,
+            },
+          })
+        )
+      }}
+    >
+      <UserCreationContainer>
+        <span className="title">ชื่อของคุณ</span>
+        <input
+          placeholder="โปรดระบุ"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+
+        <FadeUp startAnimate={startAnimate}>
+          <button type="submit">ยืนยัน</button>
+        </FadeUp>
+      </UserCreationContainer>
+    </form>
+  )
 }
 
 export default HomePage
