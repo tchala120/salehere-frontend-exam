803d2d86b38255cf8e1d1ec0189057cf05417dfd
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-01-26 17:24:26 +0700

    feat: Added route to helper to generate route easier

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,3 +1,23 @@
+import { generatePath } from 'react-router-dom'
+import queryString from 'query-string'
+
+type RouteToValue = string | number | boolean | null | undefined
+
+interface RouteToOptions {
+  params?: Record<string, RouteToValue>
+  query?: Record<string, RouteToValue>
+}
+
 export const getSearchParams = () => {
   return new URLSearchParams(window.location.search)
+}
+
+export const routeTo = (path: string, options?: RouteToOptions) => {
+  const result = generatePath(path, options?.params)
+
+  if (options?.query != null) {
+    return `${result}?${queryString.stringify(options?.query)}`
+  }
+
+  return result
 }
