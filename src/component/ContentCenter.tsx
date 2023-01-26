a7de647c916d546178e8ce516b69d3e75b3fbbc4
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-01-26 16:49:51 +0700

    feat: Added content center component

---
+++ src/component/ContentCenter.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const ContentCenter = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+`
+
+export default ContentCenter
